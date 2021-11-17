a3b73893c0548cb81df7e8e8e56e969a9b97e520
Nickgonzales76017 <45552585+Nickgonzales76017@users.noreply.github.com>
2021-11-17 17:02:15 -0600

    Update PluginEditor.cpp

--- NonlinearBiquad/Plugin/Source/PluginEditor.cpp
+++ NonlinearBiquad/Plugin/Source/PluginEditor.cpp
@@ -18,7 +18,81 @@ NonlinearBiquadAudioProcessorEditor::NonlinearBiquadAudioProcessorEditor (Nonlin
     vts (p.getVTS()),
     viewer (vts)
 {
-    setSize (400, 500);
+    const Array<AudioProcessorParameter*> parameters = processor.getParameters();
+    int comboBoxCounter = 0;
+
+    int editorHeight = 2 * editorMargin;
+    for (int i = 0; i < parameters.size(); ++i) {
+        if (const AudioProcessorParameterWithID* parameter =
+                dynamic_cast<AudioProcessorParameterWithID*> (parameters[i])) {
+
+            if (processor.parameters.parameterTypes[i] == "Slider") {
+                Slider* aSlider;
+                sliders.add (aSlider = new Slider());
+                aSlider->setTextValueSuffix (parameter->label);
+                aSlider->setTextBoxStyle (Slider::TextBoxLeft,
+                                          false,
+                                          sliderTextEntryBoxWidth,
+                                          sliderTextEntryBoxHeight);
+
+                SliderAttachment* aSliderAttachment;
+                sliderAttachments.add (aSliderAttachment =
+                    new SliderAttachment (processor.parameters.apvts, parameter->paramID, *aSlider));
+
+                components.add (aSlider);
+                editorHeight += sliderHeight;
+            }
+
+            //======================================
+
+            else if (processor.parameters.parameterTypes[i] == "ToggleButton") {
+                ToggleButton* aButton;
+                toggles.add (aButton = new ToggleButton());
+                aButton->setToggleState (parameter->getDefaultValue(), dontSendNotification);
+
+                ButtonAttachment* aButtonAttachment;
+                buttonAttachments.add (aButtonAttachment =
+                    new ButtonAttachment (processor.parameters.apvts, parameter->paramID, *aButton));
+
+                components.add (aButton);
+                editorHeight += buttonHeight;
+            }
+
+            //======================================
+
+            else if (processor.parameters.parameterTypes[i] == "ComboBox") {
+                ComboBox* aComboBox;
+                comboBoxes.add (aComboBox = new ComboBox());
+                aComboBox->setEditableText (false);
+                aComboBox->setJustificationType (Justification::left);
+                aComboBox->addItemList (processor.parameters.comboBoxItemLists[comboBoxCounter++], 1);
+
+                ComboBoxAttachment* aComboBoxAttachment;
+                comboBoxAttachments.add (aComboBoxAttachment =
+                    new ComboBoxAttachment (processor.parameters.apvts, parameter->paramID, *aComboBox));
+
+                components.add (aComboBox);
+                editorHeight += comboBoxHeight;
+            }
+
+            //======================================
+
+            Label* aLabel;
+            labels.add (aLabel = new Label (parameter->name, parameter->name));
+            aLabel->attachToComponent (components.getLast(), true);
+            addAndMakeVisible (aLabel);
+
+            components.getLast()->setName (parameter->name);
+            components.getLast()->setComponentID (parameter->paramID);
+            addAndMakeVisible (components.getLast());
+        }
+    }
+
+    //======================================
+
+    editorHeight += components.size() * editorPadding + 500;
+    editorWidth = (editorWidth + 400 + editorPadding*2)/2
+    setSize (editorWidth, editorHeight);
     addAndMakeVisible (viewer);
 
     auto setupBox = [this] (ComboBox& box, AudioProcessorValueTreeState& vts, String paramID,
@@ -103,4 +177,19 @@ void NonlinearBiquadAudioProcessorEditor::resized()
     gainSlide.setBounds    (qSlide.getRight()     - 20, 415, 80, 80);
     driveSlide.setBounds   (gainSlide.getRight()  - 20, 415, 80, 80);
     satBox.setBounds       (driveSlide.getRight() - 05, 440, 70, 20);
+  Rectangle<int> r = getLocalBounds().reduced (editorMargin);
+    r = r.removeFromRight (r.getWidth() - labelWidth);
+
+    for (int i = 0; i < components.size(); ++i) {
+        if (Slider* aSlider = dynamic_cast<Slider*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (sliderHeight));
+
+        if (ToggleButton* aButton = dynamic_cast<ToggleButton*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (buttonHeight));
+
+        if (ComboBox* aComboBox = dynamic_cast<ComboBox*> (components[i]))
+            components[i]->setBounds (r.removeFromTop (comboBoxHeight));
+
+        r = r.removeFromBottom (r.getHeight() - editorPadding);
+    }
 }
