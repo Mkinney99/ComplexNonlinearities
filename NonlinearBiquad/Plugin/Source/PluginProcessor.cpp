a40879d3fb8db41255b3168d200c64d1f0e2bd7d
Nickgonzales76017 <45552585+Nickgonzales76017@users.noreply.github.com>
2021-11-17 17:03:47 -0600

    Update PluginProcessor.cpp

--- NonlinearBiquad/Plugin/Source/PluginProcessor.cpp
+++ NonlinearBiquad/Plugin/Source/PluginProcessor.cpp
@@ -10,6 +10,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "PluginParameter.h"
 
 //==============================================================================
 NonlinearBiquadAudioProcessor::NonlinearBiquadAudioProcessor()
